f2ec45993c231ec4dc37818443fd6e8f80110f30
Leandro Damo <leandrodamo1@gmail.com>
2019-08-06 13:32:04 -0300

    Codigo de arrays de solucion

--- TP5/src/main/java/wenance_tp4/App.java
+++ TP5/src/main/java/wenance_tp4/App.java
@@ -10,25 +10,36 @@ public class App {
     public static void main(String[] args) {
         String[] arrA = {"casa", "perro", "auto", "lampara", "zapato"};
         int x = (int) (Math.random() * 5);
-        String letra = (char)perdiLetra();
+        int errores = 0;
+        char letra = perdiLetra();
         char[] caracteres = arrA[x].toCharArray();
+        char[] solucion =  new char[arrA[x].length()];
         System.out.println(Arrays.toString(caracteres));
+        boolean correcto = false;
 
-        for (int i = 0; i < arrA[x].length();i++){
-            if (letra.equals(caracteres[i])){
-                System.out.println("Letra correcta!");
-            } else
-                System.out.println("Letra incorrecta");
+        for (int i = 0; i < arrA[x].length(); i++) {
+            if (letra == caracteres[i]) {
+                correcto = true;
+                solucion[i] = letra;
+
+            }
+        }
+        System.out.println(Arrays.toString(solucion));
+        if (correcto){
+            System.out.println("Letra correcta");
+        } else {
+            System.out.println("Letra incorrecta");
+            errores++;
         }
 
 
-        }
+    }
 
-    public static String perdiLetra(){
+    public static char perdiLetra() {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Ingrese una letra");
-        String a = teclado.next();
-        return a;
+        char c = teclado.next().charAt(0);
+        return c;
     }
 
 }
