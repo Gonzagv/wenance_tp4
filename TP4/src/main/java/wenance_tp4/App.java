4a305241a594d30fb40bfc2c75420740fa0cae45
Gonzagv <gonzig1996@gmail.com>
2019-08-06 10:19:20 -0300

    Creo el maven

---
+++ TP4/src/main/java/wenance_tp4/App.java
@@ -0,0 +1,13 @@
+package wenance_tp4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
