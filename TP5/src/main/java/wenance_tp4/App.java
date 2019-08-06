583d76f32bf5af07a33e0796a62783739771da35
Gonzagv <gonzig1996@gmail.com>
2019-08-06 13:06:48 -0300

    Agrego switch del aorcado

--- TP5/src/main/java/wenance_tp4/App.java
+++ TP5/src/main/java/wenance_tp4/App.java
@@ -6,10 +6,105 @@ package wenance_tp4;
  */
 public class App 
 {
-    public static void main( String[] args )
-
-    {
-        System.out.println("hola");
+    public static void main( String[] args ) {
+        int errores = 0;
+        boolean correcto = false;
+        do{
+            for (errores = 0; errores<=6; errores++) {
+                switch (errores) {
+                    case 1:
+                        System.out.println("    ____________");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           _           ");
+                        System.out.println("   |          |_|          ");
+                        System.out.println("   |                       ");
+                        System.out.println("   |                       ");
+                        System.out.println("   |                       ");
+                        System.out.println("   |                       ");
+                        System.out.println("   |                       ");
+                        System.out.println("  ___                      ");
+                        break;
+                    case 2:
+                        System.out.println("    ____________");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           _           ");
+                        System.out.println("   |          |_|          ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |                       ");
+                        System.out.println("   |                       ");
+                        System.out.println("  ___                      ");
+                        break;
+                    case 3:
+                        System.out.println("    ____________");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           _           ");
+                        System.out.println("   |          |_|          ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |         --|           ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |                       ");
+                        System.out.println("   |                       ");
+                        System.out.println("  ___                      ");
+                        break;
+                    case 4:
+                        System.out.println("    ____________");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           _           ");
+                        System.out.println("   |          |_|          ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |         --|--           ");
+                        System.out.println("   |           |          ");
+                        System.out.println("   |                       ");
+                        System.out.println("   |                       ");
+                        System.out.println("  ___                      ");
+                        break;
+                    case 5:
+                        System.out.println("    ____________");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           _           ");
+                        System.out.println("   |          |_|          ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |         --|--           ");
+                        System.out.println("   |           |          ");
+                        System.out.println("   |          /            ");
+                        System.out.println("   |                       ");
+                        System.out.println("  ___                      ");
+                        break;
+                    case 6:
+                        System.out.println("    ____________");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |           _           ");
+                        System.out.println("   |          |_|          ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |         --|--         ");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |          - -          ");
+                        System.out.println("   |         |   |         ");
+                        System.out.println("  ___                      ");
+                        break;
+                    default:
+                        System.out.println("    ____________");
+                        System.out.println("   |           |           ");
+                        System.out.println("   |                       ");
+                        System.out.println("   |                       ");
+                        System.out.println("   |                       ");
+                        System.out.println("   |                       ");
+                        System.out.println("   |                       ");
+                        System.out.println("   |                       ");
+                        System.out.println("   |                       ");
+                        System.out.println("  ___                      ");
+                        break;
+                }
+            }
+        }while(errores <=6 || !correcto);
     }
 
 }
